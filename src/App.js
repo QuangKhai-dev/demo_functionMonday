9a72c0d43dadc405347d9bb3a95c4f46b622a5aa
Khải Đỗ <88335848+QuangKhai-dev@users.noreply.github.com>
2024-01-17 02:05:41 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,74 +1,74 @@
-import React, { useState } from 'react';
-import { Checkbox, Table } from 'antd';
+import React, { useState } from "react";
+import { Checkbox, Table } from "antd";
 const dataSource = [
   {
-    key: '1',
-    firstName: 'John',
-    lastName: 'Brown',
+    key: "1",
+    firstName: "John",
+    lastName: "Brown",
     age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
   },
   {
-    key: '2',
-    firstName: 'Jim',
-    lastName: 'Green',
+    key: "2",
+    firstName: "Jim",
+    lastName: "Green",
     age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
   },
   {
-    key: '3',
-    firstName: 'Joe',
-    lastName: 'Black',
+    key: "3",
+    firstName: "Joe",
+    lastName: "Black",
     age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
+    address: "Sydney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
   },
 ];
 export default function App() {
   const [listColumn, setListColumn] = useState([
     {
       id: 1,
-      name: 'Status',
-      type: 'Essentials',
+      name: "Status",
+      type: "Essentials",
       columnTemplateValue: [
         {
           id: 3,
-          name: 'Stuck',
+          name: "Stuck",
         },
         {
           id: 2,
-          name: 'Working on it',
+          name: "Working on it",
         },
         {
           id: 1,
-          name: 'Done',
+          name: "Done",
         },
       ],
     },
     {
       id: 2,
-      name: 'Timeline ',
-      type: 'Essentials',
+      name: "Timeline ",
+      type: "Essentials",
       columnTemplateValue: [],
     },
     {
       id: 3,
-      name: 'Numbers',
-      type: 'Essentials',
+      name: "Numbers",
+      type: "Essentials",
       columnTemplateValue: [],
     },
     {
       id: 4,
-      name: 'Text',
-      type: 'Super Useful',
+      name: "Text",
+      type: "Super Useful",
       columnTemplateValue: [],
     },
     {
       id: 5,
-      name: 'Files',
-      type: 'Super Useful',
+      name: "Files",
+      type: "Super Useful",
       columnTemplateValue: [],
     },
   ]);
@@ -82,9 +82,6 @@ export default function App() {
             <img className="h-6" src="./public/mondayS_miniLogo.png" alt="" />
           </div>
           <div className="grow-8">
-            <h1 className="text-3xl font-normal mb-5">
-              Let’s select the relevant columns for your board
-            </h1>
             <div className="flex flex-col">
               <label htmlFor="" className="text-sm mb-3">
                 Choose from the most popular column types for your work
